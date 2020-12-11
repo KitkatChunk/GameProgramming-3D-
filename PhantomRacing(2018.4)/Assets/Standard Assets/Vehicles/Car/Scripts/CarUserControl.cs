5e71e9971fe269aaddd1e4fc0764bc0d59f120b1
Karem <karemalani2402@yahoo.com>
2020-12-10 22:38:09 -0500

    Implemented Unet multiplayer

--- PhantomRacing(2018.4)/Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
+++ PhantomRacing(2018.4)/Assets/Standard Assets/Vehicles/Car/Scripts/CarUserControl.cs
@@ -1,11 +1,12 @@
 using System;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.Networking;
 
 namespace UnityStandardAssets.Vehicles.Car
 {
     [RequireComponent(typeof (CarController))]
-    public class CarUserControl : MonoBehaviour
+    public class CarUserControl : NetworkBehaviour
     {
         private CarController m_Car; // the car controller we want to use
 
@@ -17,17 +18,30 @@ namespace UnityStandardAssets.Vehicles.Car
         }
 
 
-        private void FixedUpdate()
+         void Update()
         {
             // pass the input to the car!
+            //if (!isLocalPlayer) 
+            //{
+            //    return;
+            //}
             float h = CrossPlatformInputManager.GetAxis("Horizontal");
             float v = CrossPlatformInputManager.GetAxis("Vertical");
-#if !MOBILE_INPUT
+
             float handbrake = CrossPlatformInputManager.GetAxis("Jump");
             m_Car.Move(h, v, v, handbrake);
-#else
-            m_Car.Move(h, v, v, 0f);
-#endif
+
         }
+        //private void FixedUpdate()
+        //{
+        //    // pass the input to the car!
+            
+        //    float h = CrossPlatformInputManager.GetAxis("Horizontal");
+        //    float v = CrossPlatformInputManager.GetAxis("Vertical");
+
+        //    float handbrake = CrossPlatformInputManager.GetAxis("Jump");
+        //    m_Car.Move(h, v, v, handbrake);
+
+        //}
     }
 }
